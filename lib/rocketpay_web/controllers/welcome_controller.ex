6548e762dfa0526a11aa33cf93435200f9c3fbbd
Felipe Humberto Teixeira <felipe@dinfo.com.br>
2021-02-26 00:19:30 -0300

    first commit

---
+++ lib/rocketpay_web/controllers/welcome_controller.ex
@@ -0,0 +1,24 @@
+defmodule RocketpayWeb.WelcomeController do
+  use RocketpayWeb, :controller
+
+  alias Rocketpay.Numbers
+
+  def index(conn, %{"filename" => filename}) do
+    filename
+    |> Numbers.sum_from_file()
+    |> handle_response(conn)
+  end
+
+  defp handle_response({:ok, %{result: result}}, conn) do
+    conn
+    |> put_status(:ok)
+    |> json(%{message: "Welcome to Rocketpay API. Here is your number: #{result}"})
+  end
+
+  defp handle_response({:error, result}, conn) do
+    conn
+    |> put_status(:bad_request)
+    |> put_view(RocketpayWeb.ErrorView)
+    |> render("400.json", result: result)
+  end
+end
